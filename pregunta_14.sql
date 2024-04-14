1b9e4a45dfbb626464b03651155d253028aea563
alosanchezvi <59671162+alosanchezvi@users.noreply.github.com>
2024-04-14 10:55:20 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -44,8 +44,8 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
 
-SELECT tbl1.K0, avg(tbl2.c21) 
+SELECT K0, avg(c21) 
 FROM tbl1
 JOIN tbl2 ON tbl1.K1 = tbl2.K1
 WHERE tbl1.c13 > 400
-GROUP BY tbl1.K0;
+GROUP BY K0;
